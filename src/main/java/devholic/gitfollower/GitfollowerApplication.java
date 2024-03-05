39dc69f1fca0f17fa4b83b926ddf921ca0565150
devholic22 <hyunjoon.tech@gmail.com>
2024-03-05 14:10:49 +0900

    initial commit

---
+++ src/main/java/devholic/gitfollower/GitfollowerApplication.java
@@ -0,0 +1,13 @@
+package devholic.gitfollower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitfollowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitfollowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devholic/gitfollower/GitfollowerApplicationTests.java
@@ -0,0 +1,13 @@
+package devholic.gitfollower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitfollowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
